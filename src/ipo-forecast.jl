84a208cb02b7e92a20758b8ab381cf59c8106337
Joe <joe@crimsonhexagon.us>
2018-02-18 17:36:31 -0500

    Added clean data to correct numerical columns

--- src/ipo-forecast.jl
+++ src/ipo-forecast.jl
@@ -14,5 +14,20 @@ Takes a string and removes $ and %, and replaces () with - for negative numbers.
     end
 end
 
+clean_data = function(df)
+    """
+Convert the price columns from strings to numbers.
+    """
+    @> begin
+        df
+        @transform(Offer = broadcast(x -> parse(Float64,  strip_punctuation(x)), :Offer),
+                   Opening = broadcast(x -> parse(Float64,  strip_punctuation(x)), :Opening),
+                   Close = broadcast(x -> parse(Float64,  strip_punctuation(x)), :Close),
+                   FirstDay = broadcast(x -> parse(Float64,  strip_punctuation(x)), :FirstDay),
+                   ChangeOpen = broadcast(x -> parse(Float64,  strip_punctuation(x)), :ChangeOpen),
+                   ChangeClose = broadcast(x -> parse(Float64,  strip_punctuation(x)), :ChangeClose))
+    end
+end
+
 df = CSV.read("data/ipo-data.csv")
 
